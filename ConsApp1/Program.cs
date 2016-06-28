e535626263f034e041f18dc47474cb268554ad89
Stas Chikrizov <staschikrizov@gmail.com>
2016-06-28 19:23:39 +0200

    Remove all

--- ConsApp1/Program.cs
+++ ConsApp1/Program.cs
@@ -8,31 +8,7 @@ class Program
 {
     static void Main()
     {
-        Animal dog = new Animal();
-        Animal cat = new Animal();
-        Animal fox = new Animal();
-        cat.age = 13;
-        cat.height = 20.3f;
-        cat.weight = 13.2f;
-        cat.species = "Angor";
-        dog.species = "Pitbull";
-        fox.species = "Fenik";
-        byte age = (byte)(cat.age + 1);
-        string catInf = string.Format("species: {0}\nage: {1}\nheight: {2}\nweight: {3}\n",cat.species, cat.age, cat.height, cat.weight);
-        string str1 = "string";
-        string str2 = "string";
-        string str3 = string.Copy(str1);
-        Console.WriteLine(str1.GetHashCode());
-        Console.WriteLine(str2.GetHashCode());
-        Console.WriteLine(str3.GetHashCode());
-        Console.WriteLine(catInf);
+        
         Console.ReadKey();
     }
-}
-
-class Animal
-{
-    public byte age;
-    public float height, weight;
-    public string species;
 }
