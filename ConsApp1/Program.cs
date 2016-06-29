2e0bc94a0317c5dade4b1530499d04f2e62c2018
Stas Chikrizov <staschikrizov@gmail.com>
2016-06-29 15:21:36 +0200

    5 commit

--- ConsApp1/Program.cs
+++ ConsApp1/Program.cs
@@ -8,7 +8,16 @@ class Program
 {
     static void Main()
     {
-        
+        int total = 2147483647;
+        try
+        {
+            total = checked(total * 2);
+            Console.WriteLine("Result is: {0}", total);
+        }
+        catch
+        {
+            Console.WriteLine("Overflow! Data was lost!");
+        }
         Console.ReadKey();
     }
 }
